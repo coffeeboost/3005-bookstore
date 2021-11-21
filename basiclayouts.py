74866ed7125115fc49ae96dfc9ae9fcf73794fc3
coffeeboost <gtcanada0730@gmail.com>
2021-11-20 19:13:23 -0500

    add python file

---
+++ basiclayouts.py
@@ -0,0 +1,151 @@
+"""PySide6 port of the widgets/layouts/basiclayout example from Qt v5.x"""
+import createdb
+import sys
+
+# from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (QApplication, QComboBox, QDialog,
+                               QDialogButtonBox, QGridLayout, QGroupBox,
+                               QFormLayout, QHBoxLayout, QLabel, QLineEdit,
+                               QMenu, QMenuBar, QPushButton, QSpinBox,
+                               QTextEdit, QVBoxLayout, QWidget, QTableView, QTableWidget,
+                               QTableWidgetItem)
+
+from PySide6.QtCore import QObject, Qt
+from PySide6.QtSql import (QSqlQuery, QSqlRelation, QSqlRelationalDelegate,
+                           QSqlRelationalTableModel)
+# from PySide6.QtWidgets import QApplication, QTableView
+
+
+def initializeModel(model):
+
+    model.setTable("books")
+    model.setRelation("books")
+    model.setRelation(2, QSqlRelation("authors", "id", "name")) #for col 2, join city using id select name
+
+    model.select()
+
+
+def createView(title, model):
+
+    table_view = QTableView()
+    table_view.setModel(model)
+    table_view.setItemDelegate(QSqlRelationalDelegate(table_view))
+    table_view.setWindowTitle(title)
+
+    return table_view
+
+
+class Dialog(QDialog):
+    num_grid_rows = 3
+    num_buttons = 4
+
+    def __init__(self):
+        super().__init__()
+
+        self.create_menu()
+        self.create_horizontal_group_box()
+        self.create_grid_group_box()
+        self.create_form_group_box()
+
+        big_editor = QTextEdit()
+        big_editor.setPlainText("This widget takes up all the remaining space "
+                "in the top-level layout.")
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+        model = QSqlRelationalTableModel()
+        # initializeModel(model)
+        # widget1 = createView("maybe", model)
+        widget2 = QTableWidget()
+        widget2.setColumnCount(5)
+
+        widget2.setHorizontalHeaderLabels(["pub_name", "address", "email", "account_num", "phone_num"])
+        query = QSqlQuery("SELECT pub_name, address, email, account_num, phone_num FROM publishers")
+        # rows = 1
+        while query.next():
+            # rows = widget2.rowCount()
+            print(query.value(0))
+            print(query.value(1))
+            print(query.value(2))
+            print(query.value(3))
+            print(query.value(4))
+
+            # widget2.setRowCount(rows + 1)
+            # widget2.setItem(rows, 0, QTableWidgetItem(str(query.value(0))))
+            # widget2.setItem(rows, 1, QTableWidgetItem(str(query.value(1))))
+            # widget2.setItem(rows, 2, QTableWidgetItem(str(query.value(2))))
+            # widget2.setItem(rows, 3, QTableWidgetItem(query.value(3)))
+            # widget2.setItem(rows, 4, QTableWidgetItem(query.value(4)))
+            # rows+=1
+
+
+        main_layout = QVBoxLayout()
+        main_layout.setMenuBar(self._menu_bar)
+        main_layout.addWidget(self._horizontal_group_box)
+        main_layout.addWidget(self._grid_group_box)
+        main_layout.addWidget(self._form_group_box)
+        main_layout.addWidget(big_editor)
+        main_layout.addWidget(button_box)
+        main_layout.addWidget(widget2)
+        self.setLayout(main_layout)
+
+        self.setWindowTitle("Basic Layouts")
+
+    def create_menu(self):
+        self._menu_bar = QMenuBar()
+
+        self._file_menu = QMenu("&File", self)
+        self._exit_action = self._file_menu.addAction("E&xit")
+        self._menu_bar.addMenu(self._file_menu)
+
+        self._exit_action.triggered.connect(self.accept)
+
+    def create_horizontal_group_box(self):
+        self._horizontal_group_box = QGroupBox("Horizontal layout")
+        layout = QHBoxLayout()
+
+        for i in range(Dialog.num_buttons):
+            button = QPushButton(f"Button {i + 1}")
+            layout.addWidget(button)
+
+        self._horizontal_group_box.setLayout(layout)
+
+    def create_grid_group_box(self):
+        self._grid_group_box = QGroupBox("Grid layout")
+        layout = QGridLayout()
+
+        for i in range(Dialog.num_grid_rows):
+            label = QLabel(f"Line {i + 1}:")
+            line_edit = QLineEdit()
+            layout.addWidget(label, i + 1, 0)
+            layout.addWidget(line_edit, i + 1, 1)
+
+        self._small_editor = QTextEdit()
+        self._small_editor.setPlainText("This widget takes up about two thirds "
+                "of the grid layout.")
+
+        layout.addWidget(self._small_editor, 0, 2, 4, 1)
+
+        layout.setColumnStretch(1, 10)
+        layout.setColumnStretch(2, 20)
+        self._grid_group_box.setLayout(layout)
+
+    def create_form_group_box(self):
+        self._form_group_box = QGroupBox("Form layout")
+        layout = QFormLayout()
+        layout.addRow(QLabel("Line 1:"), QLineEdit())
+        layout.addRow(QLabel("Line 2, long text:"), QComboBox())
+        layout.addRow(QLabel("Line 3:"), QSpinBox())
+        self._form_group_box.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    createdb.init_db()
+
+
+
+    dialog = Dialog()
+    sys.exit(dialog.exec())
---
+++ createdb.py
@@ -0,0 +1,162 @@
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
+from datetime import date
+
+PUBLISHERS_SQL = """
+CREATE TABLE PUBLISHERS
+(
+	pub_name	varchar(70),
+	address		varchar(200),
+	email		varchar(50),
+	account_num	numeric(12, 0) UNIQUE,
+	phone_num	numeric(10, 0) UNIQUE,
+	PRIMARY KEY (pub_name)
+);
+"""
+
+BOOKS_SQL ="""
+CREATE TABLE BOOKS
+(
+	ISBN		numeric(13, 0),
+	title		varchar(60),
+	author		varchar(60),
+	pub_name	varchar(70),
+	genre		varchar(20),
+	num_pages	numeric(3, 0),
+	price		numeric(4, 2),
+    quantity	integer,
+	sale_percent numeric (2, 0) check (sale_percent < 45),
+	PRIMARY KEY (ISBN),
+	FOREIGN KEY (pub_name) references PUBLISHERS
+		on delete cascade
+);
+"""
+USERS_SQL = """
+CREATE TABLE USERS
+(
+	username		varchar(10),
+	password		varchar(12),
+	billing_info	varchar(40),
+	shipping_info	varchar(40),
+	PRIMARY KEY (username)
+);
+"""
+
+ORDERS_SQL = """
+CREATE TABLE ORDERS
+(
+	order_id	numeric(10, 0),
+	username	varchar(10),
+	ISBN		numeric(13, 0),
+	order_date	date,
+	PRIMARY KEY (order_id, username, ISBN, order_date),
+	FOREIGN KEY (ISBN) references BOOKS
+		on delete cascade,
+	FOREIGN KEY (username) references USERS
+		on delete cascade
+);
+"""
+
+INSERT_PUBLISHERS_SQL = """
+insert into publishers(pub_name, address, email, account_num, phone_num)
+values(?, ?, ?, ?, ?)
+"""
+INSERT_BOOKS_SQL = """
+insert into books(ISBN, title, author, pub_name, genre, num_pages, price, quantity, sale_percent)
+values(?, ?, ?, ?, ?, ?, ?, ?)
+"""
+INSERT_USERS_SQL = """
+insert into users(username, password, billing_info, shipping_info)
+values(?, ?, ?, ?)
+"""
+INSERT_ORDERS_SQL = """
+insert into orders(order_id, username, ISBN, order_date)
+values(?, ?, ?, ?)
+"""
+
+
+def add_publisher(q, pub_name, address, email, account_num, phone_num):
+    q.addBindValue(pub_name)
+    q.addBindValue(address)
+    q.addBindValue(email)
+    q.addBindValue(account_num)
+    q.addBindValue(phone_num)
+    q.exec()
+
+def add_book(q, isbn, title, author, pub_name, genre, num_pages, price, quantity, sale_percent):
+    q.addBindValue(isbn)
+    q.addBindValue(title)
+    q.addBindValue(author)
+    q.addBindValue(pub_name)
+    q.addBindValue(genre)
+    q.addBindValue(num_pages)
+    q.addBindValue(price)
+    q.addBindValue(quantity)
+    q.addBindValue(sale_percent)
+    q.exec()
+
+def add_user(q, username, password, billing_info, shipping_info):
+    q.addBindValue(username)
+    q.addBindValue(password)
+    q.addBindValue(billing_info)
+    q.addBindValue(shipping_info)
+    q.exec()
+
+def add_order(q, order_id, username, ISBN, order_date):
+    q.addBindValue(order_id)
+    q.addBindValue(username)
+    q.addBindValue(ISBN)
+    q.addBindValue(order_date)
+    q.exec()
+
+def init_db():
+    def check(func, *args):
+        if not func(*args):
+            raise ValueError(func.__self__.lastError())
+    db = QSqlDatabase.addDatabase("QSQLITE")
+    db.setDatabaseName(":memory:")
+
+    check(db.open)
+
+    q = QSqlQuery()
+    check(q.exec, PUBLISHERS_SQL)
+    check(q.exec, BOOKS_SQL)
+    check(q.exec, USERS_SQL)
+    check(q.exec, ORDERS_SQL)
+
+    check(q.prepare, INSERT_PUBLISHERS_SQL)
+    add_publisher(q, 'Knopf', '1745 Broadway, New York, NY 10019', 'knopfpublicity@randomhouse.com', 101142396013, 6138904762)
+    add_publisher(q, 'Celadon Books', '120 Broadway, New York, NY 10271', 'contact@celadonbooks.com', 101142396011, 6138904381)
+    add_publisher(q, 'Broadway Books', '1714 NE Broadway, Portland, OR 97232', 'bookbroads@broadwaybooks.net', 101142396462, 6132304381)
+    add_publisher(q, 'Macmillan Australia', 'Level 25, 1 Market Street, SYDNEY NSW 2000', 'pan.reception@macmillan.com.au', 101142775462, 613237998)
+    add_publisher(q, 'Crown', '222 Rosewood Drive, Danvers, MA 01923', 'RHAcademic@penguinrandomhouse.com', 104571775462, 6135567998)
+    add_publisher(q, 'HarperCollins Publishers', '195 Broadway, New York, NY 10007', 'hello@harpercollins.com', 128571775462, 6131267008)
+    add_publisher(q, 'Little Brown Book Group', 'Carmelite House, 50 Victoria Embankment, London, EC4Y 0DZ', 'enquiries@hachette.co.uk', 128571295462, 6131219108)
+
+
+    check(q.prepare, INSERT_BOOKS_SQL)
+    add_book(q, 9781472134950, 'No Quarter Given', 'Neil Broadfoot', 'Little Brown Book Group', 'Mystery-Thriller', 304, 65.99, 10, 10)
+    add_book(q, 9780804188975, 'The Grownup', 'Gillian Flynn', 'Crown', 'Thriller', 240, 54.99, 10, 12)
+    add_book(q, 9780380731862, 'Shutter Island', 'Dennis Lehane', 'HarperCollins Publishers', 'Thriller', 369, 39.99, 10, 12)
+    add_book(q, 9781101904220, 'Dark Matter', 'Blake Crouch', 'Crown', 'Thriller', 342, 29.99, 10, 15)
+    add_book(q, 9781250105608, 'The Dry', 'Jane Harper', 'Macmillan Australia', 'Mystery', 336, 49.99, 10, 25)
+    add_book(q, 9780307269751, 'The Girl with the Dragon Tattoo', 'Stieg Larsson', 'Knopf', 'Mystery-Thriller', 480, 59.99, 10, 10)
+    add_book(q, 9781250301697, 'The Silent Patient', 'Alex Michaelides', 'Celadon Books', 'Fiction', 325, 34.99, 10, 17)
+    add_book(q, 9780307588364, 'Gone Girl', 'Gillian Flynn', 'Broadway Books', 'Thriller', 415, 24.99, 10, 30)
+    add_book(q, 9781101947807, 'Send For Me', 'Lauren Fox', 'Knopf', 'Historical Fiction', 272, 10.99, 10, 15)
+    add_book(q, 9781476746586, 'All The Light We Cannot See', 'Anthony Doerr', 'Broadway Books', 'Historical Fiction', 531, 29.99, 10, 10)
+
+
+    check(q.prepare, INSERT_USERS_SQL)
+    add_user(q, 'rm_9248', 'abc', 'Ottawa, ON, K1V6Z2', 'Ottawa, ON, K1V6Z2')
+    add_user(q, 'gordontang', 'deg', 'Toronto, ON, L4T9Y9', 'Toronto, ON, L4T9Y9')
+    add_user(q, 'tatakae', 'kill', 'Berlin, Germany, 10115', 'Berlin, Germany, 10115')
+
+
+    check(q.prepare, INSERT_ORDERS_SQL)
+    add_order(q, 1000000001, 'rm_9248', 9780804188975, '2021-11-18')
+    add_order(q, 1000000002, 'rm_9248', 9781101947807, '2021-11-18')
+
+
+
+
+init_db()
