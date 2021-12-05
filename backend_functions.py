bff8ea2cd48c7186b56d68c32ef582056e7ffe5e
Ujan Sen <55211866+ujansen@users.noreply.github.com>
2021-12-05 17:46:05 -0500

    Update backend_functions.py
    Added prepared statements for searches and user-entered values

--- backend_functions.py
+++ backend_functions.py
@@ -12,16 +12,11 @@ import calendar
 THRESHOLD_VALUE = 5
 
 
-def register(user):
-    query = QSqlQuery()
-    check(query.prepare, createdb.INSERT_USERS_SQL)
-    createdb.add_user(query, user.get('username'), user.get('password'), user.get('billing_info'), user.get('shipping_info'))
-
-
 def login(user):
     count = 0
-    username = '\'' + user.get('username') + '\''
-    query = QSqlQuery('SELECT username, password FROM USERS WHERE username = {name}'.format(name=username))
+    query = QSqlQuery('SELECT username, password FROM USERS WHERE username = ?')
+    query.addBindValue(user.get('username'))
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while logging in")
         print(query.lastError())
@@ -34,8 +29,9 @@ def login(user):
 
 
 def search(term, searchBy):
-     term = '\'' + term + '\''
-     query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price FROM books WHERE {searching} = {term}'.format(searching=searchBy, term=term))
+     query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price FROM books WHERE {searching} = ?'.format(searching=searchBy))
+     query.addBindValue(term)
+     query.exec()
      if (query.lastError().isValid()):
         print("Error while searching")
         print(query.lastError())
@@ -46,21 +42,8 @@ def search(term, searchBy):
                                  genre=str(query.value(4)), num_pages=query.value(5), price=query.value(6)))
      return dict(error=False, data=searchResults)
 
-'''
-user
-{
-    username: data,
-    password: data
-}
-CHECKOUT_CART of (book) dictionaries
-book
-{
-    title: data,
-    ISBN: data,
-    quantity: data
-}
-order_id: uuid
-'''
+
+
 def checkout(user, cart):
     if not login(user):
         return dict(error=True, data="Invalid username")
@@ -93,12 +76,17 @@ def checkout(user, cart):
         check(query.prepare, createdb.INSERT_ORDERS_SQL)
         createdb.add_order(query, order_id, user.get('username'), cart[i].get('ISBN'), dateString,
                            cart[i].get('quantity'))
-        query = QSqlQuery('UPDATE BOOKS SET quantity = quantity - {q} WHERE ISBN = {isbn}'.format(q=cart[i].get('quantity'),isbn=cart[i].get('ISBN')))
+        query = QSqlQuery('UPDATE BOOKS SET quantity = quantity - ? WHERE ISBN = ?')
+        query.addBindValue(cart[i].get('quantity'))
+        query.addBindValue(cart[i].get('ISBN'))
+        query.exec()
         if (query.lastError().isValid()):
             print("Error while changing quantity of books")
             print(query.lastError())
             return dict(error=True, data="Error while changing quantity of books. Details: {details}".format(details=query.lastError().text()))
-        query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price, quantity FROM BOOKS WHERE ISBN = {isbn}'.format(isbn=cart[i].get('ISBN')))
+        query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price, quantity FROM BOOKS WHERE ISBN =?')
+        query.addBindValue(cart[i].get('ISBN'))
+        query.exec()
         if (query.lastError().isValid()):
             print("Error while checking minimum threshold")
             print(query.lastError())
@@ -107,12 +95,14 @@ def checkout(user, cart):
             book=dict(ISBN=query.value(0), title=str(query.value(1)), author=str(query.value(2)),
                   pub_name=str(query.value(3)), genre=str(query.value(4)), num_pages=query.value(5),
                   price=query.value(6), quantity=query.value(7))
-            if (cart[i].get('quantity') < THRESHOLD_VALUE):
+            if (query.value(7) < THRESHOLD_VALUE):
                 check_threshold(book)
-            query2 = QSqlQuery('SELECT BOOKS.pub_name, account_num from BOOKS NATURAL JOIN PUBLISHERS WHERE BOOKS.ISBN = {isbn}'.format(isbn=cart[i].get('ISBN')))
+            query2 = QSqlQuery('SELECT BOOKS.pub_name, account_num from BOOKS NATURAL JOIN PUBLISHERS WHERE BOOKS.ISBN = ?')
+            query2.addBindValue(cart[i].get('ISBN'))
+            query2.exec()
             while(query2.next()):
                 publisher=dict(pub_name=query2.value(0), account_num=query2.value(1))
-                transfer_sale(book, publisher)
+                transfer_sale(book, publisher, cart[i].get('quantity'))
 
     message = "Your Order Details:\nOrder ID: {oid}\n\n".format(oid=order_id)
     for i in range(len(cart)):
@@ -120,9 +110,13 @@ def checkout(user, cart):
                                                                                          isbn=cart[i].get('ISBN'),
                                                                                          quantity=cart[i].get('quantity'))
     return dict(error=False, data=message)
+
+
 def track_order(order_id):
     query = QSqlQuery('SELECT BOOKS.ISBN, BOOKS.title, ORDERS.order_id, ORDERS.quantity FROM \
-                      ORDERS JOIN BOOKS WHERE ORDERS.order_id = {oid} AND BOOKS.ISBN=ORDERS.ISBN'.format(oid=order_id))
+                      ORDERS JOIN BOOKS WHERE ORDERS.order_id = ? AND BOOKS.ISBN=ORDERS.ISBN')
+    query.addBindValue(order_id)
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while tracking: ", order_id)
         print(query.lastError())
@@ -141,8 +135,9 @@ def owner_add_publisher(publisher):
 
 
 def owner_remove_publisher(publisher):
-    pubName = '\'' + publisher.get('pub_name') + '\''
-    query = QSqlQuery('DELETE FROM PUBLISHER WHERE pub_name = {name}'.format(name = pubName))
+    query = QSqlQuery('DELETE FROM PUBLISHER WHERE pub_name ?')
+    query.addBindValue(publisher.get('pub_name'))
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while removing: ", publisher.get('pub_name'))
         print(query.lastError())
@@ -157,7 +152,9 @@ def owner_add_book(book):
 
 
 def owner_remove_book(book):
-    query = QSqlQuery('DELETE FROM BOOKS WHERE ISBN = {isbn}'.format(isbn = book.get('ISBN')))
+    query = QSqlQuery('DELETE FROM BOOKS WHERE ISBN = ?')
+    query.addBindValue(book.get('ISBN'))
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while removing book with: ", book.get('ISBN'))
         print(query.lastError())
@@ -305,8 +302,9 @@ def display_author_report(time):
 
 def view_similar_books(book):
     count = 0
-    genreSimilar = '\'' + book.get('genre') + '\''
-    query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price FROM BOOKS WHERE genre = {g}'.format(g=genreSimilar))
+    query = QSqlQuery('SELECT ISBN, title, author, pub_name, genre, num_pages, price FROM BOOKS WHERE genre = ?')
+    query.addBindValue(book.get('genre'))
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while retrieving similar books")
         print(query.lastError())
@@ -361,8 +359,10 @@ def display_pub_report(time):
     return pubSoldPrice, pubSoldList
 
 
-def transfer_sale(book, publisher):
-    query = QSqlQuery('SELECT price, sale_percent FROM BOOKS WHERE ISBN = {isbn}'.format(isbn=book.get('ISBN')))
+def transfer_sale(book, publisher, quantity):
+    query = QSqlQuery('SELECT price, sale_percent FROM BOOKS WHERE ISBN = ?')
+    query.addBindValue(book.get('ISBN'))
+    query.exec()
     if (query.lastError().isValid()):
         print("Error while transferring sales for", book.get('title'))
         print(query.lastError())
@@ -371,11 +371,13 @@ def transfer_sale(book, publisher):
         amountTransfer = query.value(0) * (query.value(1)/100)
     return dict(error=False, data="{book_title} by Publisher {pub_title} sold\n{money:.2f}$ transferred to A/C: {account}\n".format(book_title=book.get('title'),
                                                                                                                                     pub_title=publisher.get('pub_name'),
-                                                                                                                                    money=amountTransfer,
+                                                                                                                                    money=amountTransfer * quantity,
                                                                                                                                     account=publisher.get('account_num')))
 
 def check_threshold(book):
-    query = QSqlQuery('SELECT quantity, order_date FROM ORDERS WHERE ISBN = {isbn}'.format(isbn=book.get('ISBN')))
+    query = QSqlQuery('SELECT quantity, order_date FROM ORDERS WHERE ISBN = ?')
+    query.addBindValue(book.get('ISBN'))
+    query.exec()
     saleCount = 0
     if (query.lastError().isValid()):
         print("Error while checking threshold for", book.get('title'))
